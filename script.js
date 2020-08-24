85441484364c53b581b60f3df65313cc7f9e1326
SergiyMihalev <56270505+SergiyMihalev@users.noreply.github.com>
2020-08-24 22:46:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
 const elements = document.querySelectorAll(".light__color");
-console.log(elements);
 
 function clickHandler() {
-  elements.classList.add(".red");
-
-  console.log(elements.classList);
+  for (let element of elements) {
+    element.classList.remove("active");
+  }
+  this.classList.add("active");
 }
-
-// function clickHandler() {
-//   if (elements[0].classList.contains("red")) {
-//     elements[0].classList.add("red");
-//     elements[1].classList.remove("yellow");
-//     elements[2].classList.remove("green");
-//   }
-//   if (elements[1].classList.contains("yellow")) {
-//     elements[0].classList.remove("red");
-//     elements[1].classList.add("yellow");
-//     elements[2].classList.remove(".green");
-//   }
-//   if (elements[2].classList.contains("green")) {
-//     elements[0].classList.remove("red");
-//     elements[1].classList.remove("yellow");
-//     elements[2].classList.add("green");
-//   }
-// }
 
 for (let element of elements) {
   element.onclick = clickHandler;
